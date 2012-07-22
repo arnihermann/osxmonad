a60f07bcd8d972783a33987c846fde78c4b7abb6
Brian McKenna <brian@brianmckenna.org>
2012-07-22 22:33:24 +1000

    Cabalise

--- src/Main.hs
+++ src/Main.hs
@@ -23,30 +23,30 @@ tileWideWindow width height index window =
           pos' = (pos window) { x = fromIntegral index * width, y = 22 } -- Hack: menu bar
           size' = (size window) { width = width, height = height }
 
-tileWide :: (CGPoint, CGSize) -> IO ()
-tileWide (_, CGSize screenWidth screenHeight) = do
+tile :: (CGPoint, CGSize) -> IO ()
+tile (_, CGSize screenWidth screenHeight) = do
   transitioning <- isSpaceTransitioning
-  if transitioning then
-      return ()
-  else do
-    context <- new (Windows nullPtr)
+  if transitioning
+    then return ()
+    else do
+      context <- new (Windows nullPtr)
 
-    count <- getWindows context
-    filledContext <- peek context
+      count <- getWindows context
+      filledContext <- peek context
 
-    windowsPtrs <- peekArray count . elements $ filledContext
-    windows <- mapM (\winPtr -> peek winPtr) windowsPtrs
+      windowsPtrs <- peekArray count . elements $ filledContext
+      windows <- mapM (\winPtr -> peek winPtr) windowsPtrs
 
-    namedWindows <- filterM (\win -> (peekCString . name $ win) >>= return . not . all isSpace) windows
+      namedWindows <- filterM (\win -> (peekCString . name $ win) >>= return . not . all isSpace) windows
 
-    let tileWidth = screenWidth / fromIntegral (length namedWindows)
+      let tileWidth = screenWidth / fromIntegral (length namedWindows)
 
-    let windows' = map (uncurry $ tileWideWindow tileWidth screenHeight) (zip [0..] namedWindows)
+      let windows' = map (uncurry $ tileWideWindow tileWidth screenHeight) (zip [0..] namedWindows)
 
-    mapM_ updateWindow windows'
+      mapM_ updateWindow windows'
 
-    freeWindows context
-    free context
+      freeWindows context
+      free context
 
 screen :: IO (CGPoint, CGSize)
 screen = do
@@ -63,11 +63,12 @@ screen = do
 main :: IO ()
 main = do
   hasAPI <- axAPIEnabled
-  if not hasAPI then do
+  if not hasAPI
+    then do
       hPutStrLn stderr "You need to enable access for Accessible Devices in Universal Access"
       exitWith $ ExitFailure 1
-  else do
-    s <- screen
-    forever $ do
-         tileWide s
-         threadDelay $ 1000 * 500
+    else do
+      s <- screen
+      forever $ do
+           tile s
+           threadDelay $ 1000 * 500
